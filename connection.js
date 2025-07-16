104c587c34a3150bc5773c7ccd814d6c2cb6ac6b
virus0231 <usman@youronlineconversation.com>
2025-07-16 14:30:37 +0500

    cloud sql

--- connection.js
+++ connection.js
@@ -1,20 +1,16 @@
 const { Sequelize } = require("sequelize")
 
 try {
-    // const sequelize = new Sequelize('dashboard', 'usman', 'CrY*#?!$&3002', {
-    //     host: "34.46.208.55",
-    //     dialect: 'mysql',
-    // });
-
-    const sequelize = new Sequelize('yoc', 'root', '', {
-        host: "localhost",
+    const sequelize = new Sequelize('dashboard', 'usman', 'CrY*#?!$&3002', {
+        host: "34.46.208.55",
         dialect: 'mysql',
-        logging: false
-        // logging: (sql, timing) => {
-        //     console.log('Generated SQL:', sql);
-        //     console.log('Bound Values:', timing);
-        // }
     });
+
+    // const sequelize = new Sequelize('yoc', 'root', '', {
+    //     host: "localhost",
+    //     dialect: 'mysql',
+    //     logging: false
+    // });
 
     module.exports = sequelize
 } catch (error) {
